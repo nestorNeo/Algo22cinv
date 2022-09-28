b771d13ac33f0b20283743a5c0ca15dd3a2ffe1e
nestorneo <nestordarck@gmail.com>
2022-09-27 20:40:54 -0500

    heap

--- homework1/Heaps/main.py
+++ homework1/Heaps/main.py
@@ -1,15 +1,23 @@
 from cgi import test
 import numpy as np 
 from heap import heap
+import matplotlib.pyplot as plt
+
+import time
+
+
 
 
 def startHeap(x, htype="max"):
-    HObject = heap(htype)
-    HObject.read_data(x)
-    HObject.print_container()
-    HObject.build_heap()
-    HObject.print_container()
-    print(HObject._length)
+    pass
+
+    #HObject = heap(htype)
+    #HObject.read_data(x)
+    #HObject.print_container()
+    #HObject.build_heap()
+    #HObject.print_container()
+    #print(HObject._length)
+
 
 
 if __name__ == "__main__":
@@ -17,14 +25,32 @@ if __name__ == "__main__":
     # Object heap
     
 
-    inputSize = range(10,13)
+    inputSize = range(10,10000)
 
+    plotMaxArraySizeX = []
+    plotMaxExecution  = []
+    plotMinExecution =  []
+      
     for n in inputSize:
         print("value of n", n)
         x = np.arange(n, dtype = np.int32 )
+        start = time.time()
         startHeap(x)
+        end = time.time()
+        plotMaxArraySizeX.append(n)
+        plotMaxExecution.append(end - start)
 
     for n in inputSize:
         print("value of n", n)
         x =np.arange(n, 0, -1, dtype = np.int32 )
+        start = time.time()
         startHeap(x, htype="min")
+        end = time.time()
+        plotMinExecution.append(end - start)
+
+
+    fig, (ax,ay) = plt.subplots(2, 1)
+    fig.subplots_adjust(hspace=0.5)
+    ax.plot(plotMaxArraySizeX,plotMaxExecution)
+    ay.plot(plotMaxArraySizeX,plotMinExecution)
+    plt.show()
